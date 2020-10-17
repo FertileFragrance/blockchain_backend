08d4fd45ecb85d69e0f3acecb3def7f18a58a849
fghyyds <63148552+fghyyds@users.noreply.github.com>
2020-10-17 19:21:20 +0800

    feat: 完成Merkle树的构建

--- src/main/java/com/blockchain/backend/entity/MerkleTree.java
+++ src/main/java/com/blockchain/backend/entity/MerkleTree.java
@@ -1,8 +1,91 @@
 package com.blockchain.backend.entity;
 
+import com.blockchain.backend.util.CalculateUtil;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * merkle树
  * @author 听取WA声一片
  */
+@Getter
 public class MerkleTree {
+
+    /**
+     * 根哈希
+     */
+    private TreeNode merkleRoot;
+
+    /**
+     * 树中的交易数据
+     */
+    private final List<Transaction> transactions = new ArrayList<>();
+
+    /**
+     * 树的高度（暂时用不到）
+     */
+    private int treeHeight;
+
+    public MerkleTree() {
+        Transaction coinBase = new Transaction();
+        this.transactions.add(coinBase);
+        this.merkleRoot = new TreeNode(CalculateUtil.applySha256(coinBase.generateHash()));
+        this.treeHeight = 1;
+    }
+
+    /**
+     * 新增交易记录
+     * @param transaction 新增的交易
+     */
+    public void insertTransaction(Transaction transaction) {
+        this.transactions.add(transaction);
+        this.updateTreeHeight();
+        TreeNode[] treeNodes = new TreeNode[this.transactions.size()];
+        for (int i = 0; i < treeNodes.length; i++) {
+            treeNodes[i] = new TreeNode(CalculateUtil.applySha256(this.transactions.get(i).generateHash()));
+        }
+        this.buildTree(treeNodes);
+    }
+
+    /**
+     * 更新树的高度
+     */
+    private void updateTreeHeight() {
+        for (int k = -1; ; k++) {
+            if (Math.pow(2, k) < this.treeHeight && this.treeHeight <= Math.pow(2, k + 1)) {
+                this.treeHeight = k + 2;
+                return;
+            }
+        }
+    }
+
+    /**
+     * 递归构建树
+     * @param treeNodes 树的节点
+     */
+    private void buildTree(TreeNode[] treeNodes) {
+        if (treeNodes.length == 1) {
+            this.merkleRoot = treeNodes[0];
+            return;
+        }
+        int len = (treeNodes.length + 1) / 2;
+        TreeNode[] newNodes = new TreeNode[len];
+        if ((treeNodes.length & 1) == 0) {
+            for (int i = 0; i < newNodes.length; i++) {
+                newNodes[i] = new TreeNode(CalculateUtil.applySha256
+                        (treeNodes[2 * i].getHashValue() + treeNodes[2 * i + 1].getHashValue()));
+            }
+        } else {
+            for (int i = 0; i < newNodes.length - 1; i++) {
+                newNodes[i] = new TreeNode(CalculateUtil.applySha256
+                        (treeNodes[2 * i].getHashValue() + treeNodes[2 * i + 1].getHashValue()));
+            }
+            newNodes[newNodes.length - 1] = new TreeNode(CalculateUtil
+                    .applySha256(treeNodes[treeNodes.length - 1].getHashValue()));
+        }
+        this.buildTree(newNodes);
+    }
+
 }
