8d2cf17099e7aced841d36ed547f23b74d907af1
Forcabarca-10 <825889487@qq.com>
2020-11-15 16:47:48 +0800

    完成普通的转账交易

--- src/main/java/com/blockchain/backend/pojo/chain/block/tree/transaction/Transaction.java
+++ src/main/java/com/blockchain/backend/pojo/chain/block/tree/transaction/Transaction.java
@@ -1,11 +1,16 @@
 package com.blockchain.backend.pojo.chain.block.tree.transaction;
 
 import com.alibaba.fastjson.JSON;
+import com.blockchain.backend.pojo.chain.BlockChain;
+import com.blockchain.backend.pojo.chain.block.Block;
+import com.blockchain.backend.pojo.chain.block.tree.MerkleTree;
 import com.blockchain.backend.pojo.chain.block.tree.transaction.inandout.*;
 import com.blockchain.backend.util.CalculateUtil;
 import com.blockchain.backend.util.ChainsUtil;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.*;
 
 @Setter
 @Getter
@@ -73,6 +78,88 @@ public class Transaction {
         Transaction transaction=new Transaction(new TransactionInput[]{txinput},new TransactionOutput[]{txoutput});
         transaction.SetTraction_ID();
         return  transaction;
+    }
+
+    /**
+     * 创建普通转账交易，内部逻辑如下：
+     * 1.获取余额，若不足以转账，创建交易失败
+     * 2.遍历账本，找到属于付款人的合适的金额
+     * 3.将outputs转成inputs
+     *  这里有一个重要逻辑是：当一个人A接收到一笔由B转来的比特币时，merkle树中记录了一笔交易B-A，交易类中input（从哪来）记录了B的地址，output（到哪去）记录了A
+     *  的地址。所以当A使用这笔钱转账时，产生的新交易中，会把之前交易中的output转为input,每一笔output再转成input后，就意味着被使用过了。
+     * 4创建输出，创建一个属于收款人的output
+     * 5.如果有找零，创建属于付款人的output
+     * 6.设置交易id
+     * 7.返回交易结构
+     * @param sender 付款方
+     * @param recipient 收款方
+     * @param amount 金额
+     * @param bc 区块链
+     * @return
+     */
+    public static Transaction newnormal_Transaction(String sender, String recipient, float amount, BlockChain bc){
+        Block lastblock=bc.getLastBlock();//得到最新区块
+        List<Transaction> transactionList=lastblock.getMerkleTree().getTransactions();//最新区块中的树包含了最全的交易信息，相当于最新的账本
+        //钱不够，交易失败
+        double res_amount=bc.getBalance(sender);
+        if(res_amount<amount){
+            System.out.println("余额不足，交易失败");
+            return null;
+        }
+        //遍历账本，找到足够的金额（ 即找到足够的到A中去的output，使这些output中的value和大于需要支付的amount)
+        //同时，由于后续需要把output转input，故需要记录找到的output所在的交易的id，以及在output[]中的位置
+        List<TransactionOutput> willpay=new ArrayList<TransactionOutput>();//用于存放找到的将要花费的output
+        Map<String,List<Integer>> TXID__outputIndex=new HashMap<>();
+        double value_found=0;
+        A:for(int i=0;i<transactionList.size();i++){//遍历交易
+            TransactionOutput transactionOutput[]=transactionList.get(i).getTransOutput();
+            List<Integer> outputIndex=new ArrayList<>();
+            for(int j=0;j<transactionOutput.length;j++){//遍历当前交易中的output
+                if(transactionOutput[j].getRecipientAddress()!=sender){//若不是到sender中的output就跳过
+                    continue ;
+                }
+                //是属于sender的比特币
+                willpay.add(transactionOutput[j]);
+                value_found+=transactionOutput[j].getValue();
+                outputIndex.add(j);
+                TXID__outputIndex.put(transactionList.get(i).Transaction_ID,outputIndex);
+                if(value_found>amount){
+                    break A;
+                }
+            }
+        }
+        //output转input 即建立新的input，使input中的交易id和output索引能和 willpay中的output一 一对应起来
+        TransactionInput transactionInput[]=new TransactionInput[willpay.size()];
+        Set keyset=TXID__outputIndex.keySet();
+        Iterator<String> iterator=keyset.iterator();
+        int j=0;
+        while(iterator.hasNext()){
+            String txid=iterator.next();
+            List<Integer> index=TXID__outputIndex.get(txid);
+            for(int i=0;i<index.size();i++){
+                transactionInput[j].setTransactionID(txid);
+                transactionInput[j].setOutput_INDEX(index.get(i));
+                transactionInput[j].setSenderAddress(sender);
+                j++;
+            }
+        }
+        //创建属于收款人的output,即到收款人去的output,最后一笔单独处理，因为可能会有找零
+        TransactionOutput to_recipient[]=new TransactionOutput[willpay.size()+1];//多一笔找零，找零可以是0
+        for(int i=0;i<to_recipient.length-2;i++){
+            to_recipient[i]=willpay.get(i);
+            to_recipient[i].setRecipientAddress(recipient);
+        }
+        Double change_back=value_found-amount;//找零数目
+        TransactionOutput last=willpay.get(willpay.size()-1);
+        last.setRecipientAddress(recipient);
+        last.setValue(last.getValue()-change_back);
+        to_recipient[to_recipient.length-2]=last;
+        TransactionOutput back=new TransactionOutput(change_back,sender);
+        to_recipient[to_recipient.length-1]=back;
+        //返回交易
+        Transaction transaction=new Transaction(transactionInput,to_recipient);
+        transaction.SetTraction_ID();
+        return transaction;
     }
     /**
      * 复制时调用此构造方法
